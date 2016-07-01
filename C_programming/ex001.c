841cc119d778d9307f874a38982e6d3649024694
momoko karasawa <gretel.leez@gmail.com>
2016-07-01 23:36:18 +0900

    ex001,002

---
+++ C_programming/ex001.c
@@ -0,0 +1,16 @@
+//練習問題1
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	//aaabbbcccと出力
+	printf("aaa");
+	printf("bbb");
+	printf("ccc\n");
+
+	/* AAA
+		 BBB
+		 CCC
+		 と出力 */
+	printf("AAA\nBBB\nCCC\n");
+	return 0;
+}
