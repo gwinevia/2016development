435799803841bb0ee3a4985912070168a898b253
momoko karasawa <gretel.leez@gmail.com>
2016-07-05 23:44:33 +0900

    読み込むデフォルトファイルを変更

--- idea/src/dash/DashDefaults.java
+++ idea/src/dash/DashDefaults.java
@@ -7,8 +7,8 @@ import java.io.*;
 public class DashDefaults {
 
   // デフォルトファイルのファイル名
-  //private static final String DEFAULTSFILE = "defaults.txt";
-  private static final String DEFAULTSFILE = "properties/simulator.properties";
+  private static final String DEFAULTSFILE = "defaults.txt";
+  //private static final String DEFAULTSFILE = "properties/simulator.properties";
 
 
   // DASHのディレクトリ
