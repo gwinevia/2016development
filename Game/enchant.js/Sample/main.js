6efa817859ffaa1b094b35aee310a2817c583435
momoko karasawa <gretel.leez@gmail.com>
2016-11-11 16:18:19 +0900

    画像を表示

---
+++ Game/enchant.js/Sample/main.js
@@ -0,0 +1,26 @@
+enchant();
+
+/*
+
+Core
+- rootScene
+-- Sprite (bear)
+
+*/
+
+window.onload = function(){
+
+    var core = new Core(320, 320);
+    core.preload('chara1.png');
+    core.onload = function(){
+
+        var bear = new Sprite(32,32);
+        bear.image = core.assets['chara1.png'];
+        bear.x = 0;
+        bear.y = 0;
+
+        core.rootScene.addChild(bear);
+    }
+
+    core.start();
+};
