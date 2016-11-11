728d65ec673bc9003de599078af24a88ec11a6df
momoko karasawa <gretel.leez@gmail.com>
2016-11-11 13:54:52 +0900

    スコア管理

--- Game/Kuma/main.js
+++ Game/Kuma/main.js
@@ -2,4 +2,38 @@ enchant();
 
 window.onload = function(){
 
+    var core = new Core(320,320);
+    core.preload('chara1.png');
+    core.fps = 15;
+
+    var score = 0;
+
+    core.onload = function(){
+        var bear = new Sprite(32,32);
+        bear.x = rand(320);
+        bear.y = rand(320);
+        bear.frame = 0;
+        bear.image = core.assets['chara1.png'];
+        
+        bear.on('touchstart',function(){
+            score++;
+            scoreLabel.text = 'Score: ' + score;
+            this.x = rand(320);
+            this.y = rand(320);
+        })
+
+        var scoreLabel = new Label('Score: 0');
+        scoreLabel.x = 200;
+        scoreLabel.y = 5;
+
+        core.rootScene.addChild(scoreLabel);
+        core.rootScene.addChild(bear);
+    }
+
+    core.start();
+
+}
+
+function rand(n){
+    return Math.floor(Math.random()*(n+1));
 }
