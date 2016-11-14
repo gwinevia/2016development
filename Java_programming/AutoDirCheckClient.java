c9918bcd84a61a06546934a8718eb2ed4a41472a
momoko karasawa <gretel.leez@gmail.com>
2016-11-14 14:59:57 +0900

    update

--- Java_programming/AutoDirCheckClient.java
+++ Java_programming/AutoDirCheckClient.java
@@ -1,10 +1,11 @@
 public class AutoDirCheckClient {
   public static void main(String[] args) {
     AutoDirCheck check = new AutoDirCheck();
+
     check.start();
     try {
       // 1分間プログラムを走らせる
-      Thread.sleep(60 * 1000L);
+      Thread.sleep(600 * 1000L);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
